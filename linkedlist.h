85a71e5329b8ec843675480e3e1f7d168d857118
“[Steven <stevenschulze7@gmail.com>
2018-12-23 01:48:48 -0800

    new files

--- linkedlist.h
+++ linkedlist.h
@@ -3,3 +3,15 @@ struct node
 	int data;
 	node* next;
 };
+
+
+class list
+{
+	private:
+	node* head;
+
+	public:
+	list();
+	node* insert(node* head);
+	node* remove(node* head);
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include "linkedlist.h"
 using namespace std;
 
 int main() 
